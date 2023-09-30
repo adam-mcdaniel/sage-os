d86b24bc4fc255bec080b314dff126fbe783169f
Jongheon Park <jpark78@vols.utk.edu>
2023-09-30 18:35:07 -0400

    Fix PCIDevice helpers' return types

--- src/pci.c
+++ src/pci.c
@@ -117,11 +117,11 @@ PCIDevice *pci_find_device_by_irq(uint8_t irq) {
 struct VirtioPciCommonCfg *pci_get_virtio_common_config(PCIDevice *device) {
     return (struct VirtioPciCommonCfg *)pci_get_virtio_capability(device, VIRTIO_PCI_CAP_COMMON_CFG);
 }
-struct VirtioPciNotifyCfg *pci_get_virtio_notify_capability(PCIDevice *device) {
-    return (struct VirtioPciNotifyCfg *)pci_get_virtio_capability(device, VIRTIO_PCI_CAP_NOTIFY_CFG);
+struct VirtioPciNotifyCap *pci_get_virtio_notify_capability(PCIDevice *device) {
+    return (struct VirtioPciNotifyCap *)pci_get_virtio_capability(device, VIRTIO_PCI_CAP_NOTIFY_CFG);
 }
-struct VirtioPciISRStatus *pci_get_virtio_isr_status(PCIDevice *device) {
-    return (struct VirtioPciISRStatus *)pci_get_virtio_capability(device, VIRTIO_PCI_CAP_ISR_CFG);
+struct VirtioPciIsrCap *pci_get_virtio_isr_status(PCIDevice *device) {
+    return (struct VirtioPciIsrCap *)pci_get_virtio_capability(device, VIRTIO_PCI_CAP_ISR_CFG);
 }
 
 static uint8_t next_bus_number = 1;
