bb4f0438878099491e3b29ae4ccfa4fd4d598d56
Jongheon Park <jpark78@vols.utk.edu>
2023-09-30 17:05:18 -0400

    Add initial implementation of virtio_notify

--- src/include/virtio.h
+++ src/include/virtio.h
@@ -37,7 +37,7 @@ typedef struct VirtioCapability {
     // What type of capability is this? (identifies the structure)
     // This will be one of 1, 2, 3, 4, or 5, but we only care about 1, 2, or 3
     uint8_t type;
-    // Which BAR is this for?
+    // Which BAR is this for? (0x0 - 0x5)
     uint8_t bar;
     // Padding to fill the next 3 bytes
     uint8_t pad[3];
@@ -156,7 +156,7 @@ typedef struct VirtioDeviceRing {
     // uint16_t     avail_event;
 } VirtioDeviceRing;
 
-struct PciDevice;
+struct PCIDevice;
 struct List;
 
 // This is the actual Virtio device structure that the OS will
@@ -165,14 +165,11 @@ struct List;
 typedef struct VirtioDevice {
     // A pointer the PCI device structure for this Virtio device.
     // Right now this just points to the ECAM for the device.
-    struct PciDevice *pcidev;
-    // The common configuration for the device.
+    struct PCIDevice *pcidev;
+    
     volatile VirtioPciCommonCfg *common_cfg;
-    // The notify register for the device. This is not the notify
-    // structure; we go ahead and do the address calculation before
-    // storing the pointer here.
-    volatile char *notify;
-    // The ISR capability for the device.
+    volatile VirtioPciNotifyCap *notify_cap;
+    // volatile uint16_t *notify;
     volatile VirtioPciIsrCap *isr;
 
     // The descriptor ring for the device.
@@ -189,7 +186,7 @@ typedef struct VirtioDevice {
     uint16_t driver_idx;
     uint16_t device_idx;
 
-    uint16_t notifymult;
+    // uint16_t notifymult;
     bool     ready;
 } VirtioDevice;
 
--- src/virtio.c
+++ src/virtio.c
@@ -1,3 +1,4 @@
+#include <debug.h>
 #include <virtio.h>
 #include <util.h>
 #include <vector.h>
@@ -5,6 +6,9 @@
 
 static Vector *virtio_devices = NULL;
 
+/**
+ * @brief Initialize the virtio system
+ */
 void virtio_init(void) {
     virtio_devices = vector_new();
 
@@ -19,6 +23,35 @@ void virtio_init(void) {
     // }
 }
 
-void virtio_notify(VirtioDevice *viodev, uint16_t which_queue) {
+/**
+ * @brief Virtio notification
+ * @param viodev - virtio device to notify for
+ * @param which_queue - queue number to notify
+ */
+void virtio_notify(VirtioDevice *viodev, uint16_t which_queue)
+{
+    uint16_t num_queues = viodev->common_cfg->num_queues;
 
-}
+    if (which_queue >= viodev->common_cfg->num_queues) {
+        logf(LOG_ERROR, "virtio_notify: Provided queue number is too big...\n");
+        return;
+    }
+
+    // Select the queue we are looking at
+    viodev->common_cfg->queue_select = which_queue;
+
+    // Determine offset for the given queue
+    uint8_t bar_num = viodev->notify_cap->cap.bar;
+    uint32_t offset = viodev->notify_cap->cap.offset;
+    uint16_t queue_notify_off = viodev->common_cfg->queue_notify_off;
+    uint32_t notify_off_multiplier = viodev->notify_cap->notify_off_multiplier;
+    uint32_t bar = viodev->pcidev->ecam_header->type0.bar[bar_num];
+
+    // FIXME: 
+    uint16_t *notify = bar + BAR_NOTIFY_CAP(offset, queue_notify_off, notify_off_multiplier);
+
+    // Write the queue's number to the notify register
+    *notify = which_queue; 
+
+    return;
+};
