efd74048b674bf5e01104483173f0ed43f2de798
Adam McDaniel <amcdan23@vols.utk.edu>
2023-10-24 23:02:57 -0400

    Added trap frame setup

--- src/include/trap.h
+++ src/include/trap.h
@@ -1,9 +1,19 @@
 // #include <asm/trampoline.S>
 
-extern uint64_t spawn_start;
-extern uint64_t spawn_end;
-
-extern uint64_t _spawn_kthread;
-extern uint64_t _trap_end;
+extern uint64_t trampoline_thread_start;
+extern uint64_t trampoline_trap_start;
 
 void os_trap_handler(void);
+
+typedef struct Trapframe {
+    uint64_t  gpregs[32]; 
+    double    fpregs[32]; 
+    uint64_t  sepc;     
+    uint64_t  sstatus;  
+    uint64_t  sie;      
+    uint64_t  satp;     
+    uint64_t  sscratch; 
+    uint64_t  stvec;    
+    uint64_t  trap_satp;
+    uint64_t  trap_stack;
+} Trapframe;
