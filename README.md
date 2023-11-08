75c05f141ef03576fe48d20657de14703c25d9ff
ttahmid <ttahmid@vols.utk.edu>
2023-11-07 20:30:29 -0500

    functions for processes and scheduling

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Every week, you will need to write entries in this journal. Include brief inform
 * any complications that you've fixed and how you did it.
 
 Sort your entries in descending order (newest entries at the top).
+# 7-Nov-2023
+- `ttahmid`: Added `Resource Control Block` and unique id generation to processes. Added more functions for scheduling like function to choose the next process to run based on CFS, `sched_choose_next`; Function to update the vruntime of a process, `sched_update_vruntime`; Function to handle the timer interrupt for context switching, `sched_handle_timer_interrupt`; as well as template functions for `context_switch`.
 
 # 5-Nov-2023
 - `amcdan23`: Expanded the block driver to work with any block device connected to the machine. Added functionality for: mounting devices on the VFS, opening `File*` objects on arbitrary mounted devices on the VFS (multiple open files on multiple drives simultaneously), reading/writing files on VFS, and automatically populating mounted drives in `/dev` folder on the base drive \#0 at `/`. Wrote `vfs_read`, `vfs_write`, `vfs_init`, `vfs_open`, `vfs_close`, `vfs_mount`, `vfs_print_mounted_devices`, `vfs_print_open_files`, many more private VFS helper functions in `vfs.c`, and rewrote all the block driver functions with devices as parameters in `block.c`. Added a PCI function for getting the Nth device of a given type.
--- src/sched.c
+++ src/sched.c
@@ -5,9 +5,12 @@ Scheduler - uses completely fair scheduler approach
 #include <process.h>
 #include <rbtree.h>
 #include <sched.h>
+#include <stddef.h>
+#include <stdint.h>
 
 
 static RBTree *sched_tree;
+
 
 //initialize scheduler tree
 void sched_init(){
@@ -15,13 +18,16 @@ void sched_init(){
 }
 
 //adds node to scheduler tree
-void sched_add(struct process *p){
-    //ensure runtime is a unique key
-    struct process *curr_process;
-    while(rb_find(sched_tree, p->runtime, curr_process)){
+void sched_add(struct process *p) {
+    struct process *curr_process = NULL;
+    uint64_t curr_process_key;
+
+    while(rb_find(sched_tree, p->runtime, &curr_process_key)) {
+        curr_process = (struct process *)curr_process_key;
         p->runtime++;
     }
-    rb_insert(sched_tree, p->runtime, p);
+    uint64_t process_key = (uint64_t)p;
+    rb_insert(sched_tree, p->runtime, process_key);
 }
 
 //removes node from scheduler tree - used if process gets manually killed
@@ -35,3 +41,66 @@ struct process *sched_get_next(){
     rb_min_val_ptr(sched_tree, min_process);
     return min_process;
 }
+
+// Function to choose the next process to run based on CFS
+struct process *sched_choose_next() {
+    struct process *next_process;
+    if (rb_min_val_ptr(sched_tree, &next_process)) {
+        return next_process;
+    }
+    return NULL; // No process is ready to run
+}
+
+// Function to update the vruntime of a process
+void sched_update_vruntime(struct process *p, unsigned long delta_time) {
+    p->runtime += delta_time;
+    // Rebalance the tree if necessary
+    rb_delete(sched_tree, p->runtime);
+    rb_insert(sched_tree, p->runtime, (uint64_t)p);
+}
+
+// Function to handle the timer interrupt for context switching
+void sched_handle_timer_interrupt() {
+    struct process *current_process = sched_get_current(); // Implement this function to get the currently running process
+    unsigned long time_slice = get_time_slice(); // Implement this function based on the timer configuration
+    sched_update_vruntime(current_process, time_slice);
+    struct process *next_process = sched_choose_next();
+    if (next_process != current_process) {
+        context_switch(current_process, next_process); // Implement context_switch function
+    }
+}
+
+void context_switch(struct process *from, struct process *to) {
+    // Save the state of the current process
+    save_state(&from->frame);
+
+    // Load the state of the next process
+    load_state(&to->frame);
+
+    // Update the current process pointer
+    set_current_process(to);
+
+    // Perform the actual switch
+    switch_to(&to->frame);
+}
+
+
+void save_state(struct trap_frame *state) {
+}
+
+void load_state(struct trap_frame *state) {
+}
+
+void switch_to(struct trap_frame *state) {
+}
+
+struct process *sched_get_current(void) {
+}
+
+unsigned long get_time_slice(void) {
+}
+
+void set_current_process(struct process *proc) {
+}
+
+
