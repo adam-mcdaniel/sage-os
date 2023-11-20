87932dfca7ad43689d6ac14db49c6dec0dbc3df2
Jongheon Park <jpark78@vols.utk.edu>
2023-11-19 22:10:22 -0500

    Map trap_frame to user's page table

--- src/trap.c
+++ src/trap.c
@@ -76,7 +76,7 @@ void os_trap_handler(void)
                 // Ack timer will reset the timer to INFINITE
                 // In src/sbi.c
                 debugf("os_trap_handler: Supervisor timer interrupt!\n");
-                CSR_CLEAR("sip");
+                // CSR_CLEAR("sip");
                 sbi_ack_timer();
                 // We typically invoke our scheduler if we get a timer
                 sched_handle_timer_interrupt(hart);
