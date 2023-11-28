b2dc48cf652b7860cdbe8645663fd3a30a529b91
Adam McDaniel <amcdan23@vols.utk.edu>
2023-11-28 03:45:28 -0500

    Fixed bug with minix3 driver causing large files to wrap in the middle

--- src/main.c
+++ src/main.c
@@ -262,7 +262,7 @@ void main(unsigned int hart)
     // vfs_close(file2);
 
     // Read in /home/cosc562/console.elf
-    File *elf_file = vfs_open("/home/cosc562/calculator.elf", 0, O_RDONLY, VFS_TYPE_FILE);
+    File *elf_file = vfs_open("/home/cosc562/hex_editor.elf", 0, O_RDONLY, VFS_TYPE_FILE);
     Stat stat;
     vfs_stat(elf_file, &stat);
     logf(LOG_INFO, "Console file size: %lu\n", stat.size);
@@ -284,7 +284,7 @@ void main(unsigned int hart)
     kernel_trap_frame->sie = SIE_STIE | SIE_SSIE | SIE_SEIE;
     CSR_WRITE("sie", kernel_trap_frame->sie);
 
-    // process_run(p, 0);
+    process_run(p, 0);
 
     console();
 #else
