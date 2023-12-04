9484b0d45e9340b49370378bc846575a35a53b98
Adam McDaniel <amcdan23@vols.utk.edu>
2023-12-04 00:29:32 -0500

    Fixed input driver bug

--- src/input.c
+++ src/input.c
@@ -16,7 +16,7 @@
 #include <compiler.h>
 #include <csr.h>
 
-#define INPUT_DEBUG
+// #define INPUT_DEBUG
 #ifdef INPUT_DEBUG
 #define debugf(...) debugf(__VA_ARGS__)
 #else
@@ -36,14 +36,14 @@ void input_device_init(VirtioDevice *device) {
     volatile VirtioInputConfig *config = virtio_get_input_config(device);
 
     uint16_t prod_id = input_device_get_prod_id(config, device->name);
-    infof("input_device_init: prod_id = 0x%x\n", prod_id);
+    debugf("input_device_init: prod_id = 0x%x\n", prod_id);
     InputDevice *input_dev = NULL;
     switch (prod_id) {
     case EV_KEY:
         // virtio_set_device_name(device, "Keyboard");
         input_dev = &keyboard_dev;
-        device->is_keyboard = 0;
-        device->is_tablet = 1;
+        device->is_keyboard = 1;
+        device->is_tablet = 0;
         keyboard_dev.viodev = device;
         break;
     case EV_ABS:
@@ -65,7 +65,7 @@ void input_device_init(VirtioDevice *device) {
     input_devices_initialized++;
 
     input_device_receive_buffer_init(input_dev);
-    infof("Input device init done for device at %p\n", device->pcidev->ecam_header);
+    debugf("Input device init done for device at %p\n", device->pcidev->ecam_header);
 }
 
 bool is_initialized() {
@@ -87,10 +87,10 @@ void input_device_receive_buffer_init(InputDevice *input_dev) {
 void get_input_device_config(VirtioDevice *device, uint8_t select, uint8_t subsel, uint8_t size) {
     volatile VirtioInputConfig *config = virtio_get_input_config(device);
     config->select = select;
-    infof("set_input_device_config: select = 0x%x\n", config->select);
+    debugf("set_input_device_config: select = 0x%x\n", config->select);
     config->subsel = subsel;
-    infof("set_input_device_config: subsel = 0x%x\n", config->subsel);
-    infof("%.128s\n", config->string);
+    debugf("set_input_device_config: subsel = 0x%x\n", config->subsel);
+    debugf("%.128s\n", config->string);
 }
 
 uint16_t input_device_get_prod_id(volatile VirtioInputConfig *config, char *name) {
@@ -108,24 +108,23 @@ void input_device_push_event(InputDevice *input_dev, VirtioInputEvent event) {
         warnf("input_device_push_event: Input device not initialized\n");
         return;
     } else if (event.type == 0 && event.code == 0 && event.value == 0) {
-        warnf("input_device_push_event: Invalid input event: type = 0x%x, code = 0x%x, value = 0x%x\n", event.type, event.code, event.value);
+        debugf("input_device_push_event: Invalid input event: type = 0x%x, code = 0x%x, value = 0x%x\n", event.type, event.code, event.value);
         return;
     } else {
-        infof("Pushing event to %s\n", input_dev->viodev->name);
+        debugf("Pushing event to %s\n", input_dev->viodev->name);
     }
 
     if (input_dev->buffer_count < INPUT_EVENT_BUFFER_SIZE) {
         ++input_dev->buffer_count;
         input_dev->event_buffer[input_dev->buffer_tail % INPUT_EVENT_BUFFER_SIZE] = event;
         input_dev->buffer_tail = (input_dev->buffer_tail + 1) % INPUT_EVENT_BUFFER_SIZE;
-        infof("input_device_isr: Input event received: type = 0x%x, code = 0x%x, value = 0x%x\n", event.type, event.code, event.value);
+        debugf("input_device_isr: Input event received: type = 0x%x, code = 0x%x, value = 0x%x\n", event.type, event.code, event.value);
     } else {
         // Just wrap around the buffer
-        ++input_dev->buffer_count;
         input_dev->event_buffer[input_dev->buffer_tail % INPUT_EVENT_BUFFER_SIZE] = event;
         input_dev->buffer_tail = (input_dev->buffer_tail + 1) % INPUT_EVENT_BUFFER_SIZE;
         input_dev->buffer_head = (input_dev->buffer_head + 1) % INPUT_EVENT_BUFFER_SIZE;
-        infof("input_device_isr: Input event received: type = 0x%x, code = 0x%x, value = 0x%x\n", event.type, event.code, event.value);
+        debugf("input_device_isr: Input event received: type = 0x%x, code = 0x%x, value = 0x%x\n", event.type, event.code, event.value);
     }
 }
 
@@ -139,20 +138,36 @@ VirtioInputEvent input_device_get_next_event(InputDevice *input_dev) {
         return event;
     }
     VirtioInputEvent event;
-    // if (input_dev->buffer_count == 0) {
-    //     warnf("input_device_get_next_event: Input event buffer empty\n");
-    //     event.type = 0;
-    //     event.code = 0;
-    //     event.value = 0;
-    //     return event;
-    // }
+    if (input_dev == NULL) {
+        fatalf("input_device_get_next_event: Input device not initialized\n");
+    }
 
-    // Get the next event from the head of the buffer
-    event = input_dev->event_buffer[input_dev->buffer_head];
-    input_dev->buffer_head = (input_dev->buffer_head + 1) % INPUT_EVENT_BUFFER_SIZE;
-    --input_dev->buffer_count;
-    infof("input_device_buffer_pop: %.60s popped event: type = 0x%x, code = 0x%x, value = 0x%x\n", input_dev->viodev->name, event.type, event.code, event.value);
-
+    if (input_dev->buffer_count <= 0) {
+        // Get the next event from the head of the buffer
+        // event = input_dev->event_buffer[input_dev->buffer_head];
+        // input_dev->buffer_head = (input_dev->buffer_head + 1) % INPUT_EVENT_BUFFER_SIZE;
+        // if (input_dev->buffer_count == 0) {
+        //     input_dev->buffer_head = 0;
+        //     input_dev->buffer_tail = 0;
+        // } else {
+        //     --input_dev->buffer_count;
+        // }
+        debugf("input_device_get_next_event: %.60s popped event: type = 0x%x, code = 0x%x, value = 0x%x\n", input_dev->viodev->name, event.type, event.code, event.value);
+        memset(&event, 0, sizeof(VirtioInputEvent));
+        event = input_dev->event_buffer[input_dev->buffer_head % INPUT_EVENT_BUFFER_SIZE];
+        input_dev->buffer_head = 0;
+        input_dev->buffer_tail = 0;
+        event.type = 0;
+        event.code = 0;
+        event.value = 0;
+        return event;
+    } else {
+        // Get the next event from input_device_isr: Received invalid input event: tthe head of the buffer
+        event = input_dev->event_buffer[input_dev->buffer_head % INPUT_EVENT_BUFFER_SIZE];
+        input_dev->buffer_head = (input_dev->buffer_head + 1) % INPUT_EVENT_BUFFER_SIZE;
+        --input_dev->buffer_count;
+        debugf("input_device_get_next_event: %.60s popped event: type = 0x%x, code = 0x%x, value = 0x%x\n", input_dev->viodev->name, event.type, event.code, event.value);
+    }
     return event;
 }
 
@@ -172,7 +187,7 @@ void handle_input(InputDevice *device) {
         warnf("input_device_isr: Device not ready!\n");
         return;
     }
-    infof("input_device_isr: Starting ISR for %.60s\n", viodev->name);
+    debugf("input_device_isr: Starting ISR for %.60s\n", viodev->name);
     
 
     uint16_t start_device_idx = viodev->device_idx;
@@ -183,41 +198,59 @@ void handle_input(InputDevice *device) {
         // uint32_t id = viodev->device->ring[viodev->device_idx % queue_size].id;
         volatile VirtioDescriptor received_desc = virtio_receive_one_descriptor(viodev, 0, true);
         volatile VirtioInputEvent *event_ptr = (volatile VirtioInputEvent *)received_desc.addr;
-        infof("About to read input event from descriptor at %p\n", received_desc.addr);
+        debugf("About to read input event from descriptor at %p\n", received_desc.addr);
+        VirtioInputEvent event;
+        if (event_ptr != NULL) {
+            event = *event_ptr;
+        }
 
         uint32_t len = received_desc.len;
         if (len != sizeof(VirtioInputEvent)) {
-            // infof("input_device_isr: Received invalid input event size: %d\n", len);
+            // debugf("input_device_isr: Received invalid input event size: %d\n", len);
             virtio_send_one_descriptor(viodev, 0, received_desc, true);
             continue;
         }
 
-        if (event_ptr->type == 0 && event_ptr->code == 0 && event_ptr->value == 0) {
-            infof("input_device_isr: Received invalid input event: type = 0x%x, code = 0x%x, value = 0x%x\n", event_ptr->type, event_ptr->code, event_ptr->value);
-            virtio_send_one_descriptor(viodev, 0, received_desc, true);
-            continue;
-        }
-
+        // if (event_ptr->type == 0 && event_ptr->code == 0 && event_ptr->value == 0) {
+        //     debugf("input_device_isr: Received invalid input event: type = 0x%x, code = 0x%x, value = 0x%x\n", event_ptr->type, event_ptr->code, event_ptr->value);
+        //     virtio_send_one_descriptor(viodev, 0, received_desc, true);
+        //     continue;
+        // }
+        
         // volatile InputDevice *input_dev;
-        if (event_ptr->type == EV_KEY) {
+        if (viodev->is_keyboard) {
             // This is a key event
             // input_dev = &keyboard_dev;
-            input_device_push_event(&keyboard_dev, *event_ptr);
+            input_device_push_event(&keyboard_dev, event);
             // input_device_push_event(&tablet_dev, *event_ptr);
         } else {
             // This is a tablet event
             // input_dev = &tablet_dev;
             // input_device_push_event(&keyboard_dev, *event_ptr);
-            input_device_push_event(&tablet_dev, *event_ptr);
+            input_device_push_event(&tablet_dev, event);
         }
 
-        infof("input_device_isr: Input event received: type = 0x%x, code = 0x%x, value = 0x%x\n", event_ptr->type, event_ptr->code, event_ptr->value);
+        debugf("input_device_isr: Input %.60s event received: type = 0x%x, code = 0x%x, value = 0x%x\n", viodev->name, event_ptr->type, event_ptr->code, event_ptr->value);
+        virtio_send_one_descriptor(viodev, 0, received_desc, true);
     }
 }
 
 void input_device_isr(VirtioDevice* viodev) {
-    handle_input(&keyboard_dev);
-    handle_input(&tablet_dev);
+    if (viodev == NULL) {
+        warnf("input_device_isr: Input device not initialized\n");
+        return;
+    }
+
+    if (viodev == keyboard_dev.viodev) {
+        handle_input(&keyboard_dev);
+    } else if (viodev == tablet_dev.viodev) {
+        handle_input(&tablet_dev);
+    } else {
+        warnf("input_device_isr: Invalid virtio device\n");
+        return;
+    }
+    // handle_input(&keyboard_dev);
+    // handle_input(&tablet_dev);
 }
 
 // Pop the input event from the event buffer ot input_dev to event.
@@ -234,11 +267,11 @@ bool input_device_buffer_pop(InputDevice *input_dev, VirtioInputEvent *event) {
     }
 
     // Event buffer is empty
-    if (input_dev->buffer_count == 0) {
+    if (input_dev->buffer_count <= 0) {
         return false;
     }
 
-    *event = input_dev->event_buffer[input_dev->buffer_tail];    
+    *event = input_dev->event_buffer[input_dev->buffer_tail % INPUT_EVENT_BUFFER_SIZE];    
     input_dev->buffer_tail = (input_dev->buffer_tail + 1) % INPUT_EVENT_BUFFER_SIZE;
     --input_dev->buffer_count;
     
@@ -247,6 +280,15 @@ bool input_device_buffer_pop(InputDevice *input_dev, VirtioInputEvent *event) {
 
 
 InputDevice *input_device_get_keyboard() {
+    if (!is_initialized()) {
+        warnf("input_device_get_tablet: Input device not initialized\n");
+        return NULL;
+    }
+    // if (!keyboard_dev.viodev || !keyboard_dev.viodev->ready) {
+    //     warnf("input_device_get_tablet: Invalid virtio device\n");
+    //     return NULL;
+    // }
+
     return &keyboard_dev;
 }
 
@@ -255,18 +297,20 @@ InputDevice *input_device_get_tablet() {
         warnf("input_device_get_tablet: Input device not initialized\n");
         return NULL;
     }
-    if (!tablet_dev.viodev || !tablet_dev.viodev->ready) {
-        warnf("input_device_get_tablet: Invalid virtio device\n");
-        return NULL;
-    }
+    // if (!tablet_dev.viodev || !tablet_dev.viodev->ready) {
+    //     warnf("input_device_get_tablet: Invalid virtio device\n");
+    //     return NULL;
+    // }
 
     return &tablet_dev;
 }
 
 VirtioInputEvent keyboard_get_next_event() {
+    debugf("keyboard_get_next_event: Getting next event\n");
     return input_device_get_next_event(&keyboard_dev);
 }
 
 VirtioInputEvent tablet_get_next_event() {
+    debugf("tablet_get_next_event: Getting next event\n");
     return input_device_get_next_event(&tablet_dev);
 }
